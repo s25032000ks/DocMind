ad1450f0c39ca0300740650b8fbd58ce696dd90a
Kumar Singh, Shubham <shubham.kumarsingh@fujitsu.com>
2026-09-18 17:18:49 +0530

    Changing the Slf4j default logger to LoggerFactory

--- src/main/java/com/ai/docMind/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ai/docMind/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.ai.docMind.exception;
 
 import com.ai.docMind.dto.ApiResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -15,8 +16,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse> handleResourceNotFound(ResourceNotFoundException exception){
